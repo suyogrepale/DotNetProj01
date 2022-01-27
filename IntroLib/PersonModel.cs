d54465bed84f1f9cdc56c3638c68d3824c62b1a4
suyogrepale <suyogbrepale@gmail.com>
2022-01-28 04:01:51 +0530

    first commit

---
+++ IntroLib/PersonModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace IntroLib
+{ 
+    public class PersonModel
+    {
+        public string FirstName { get; set; }   
+        public string LastName { get; set; }
+        public int Age { get; set; }
+
+    }
+    
+}
---
+++ IntroUI/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using IntroLib;
+
+namespace Program // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PersonModel p = new PersonModel{
+                FirstName="Suyog",
+                LastName = "Repale"
+            };
+
+            Console.WriteLine($"{p.FirstName} {p.LastName} is my name.");
+
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Hello Suyog!");
+
+            Console.WriteLine("Hello Kaveron India Pvt Ltd!");
+        }
+    }
+}
